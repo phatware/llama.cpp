d905cbf566bf86c0aa75828c95de811ec63d333a
Stan <stanmiasnikov@gmail.com>
2025-01-04 23:44:26 -0800

    Package updated

--- Package.swift
+++ Package.swift
@@ -7,17 +7,29 @@ var sources = [
     "src/llama-vocab.cpp",
     "src/llama-grammar.cpp",
     "src/llama-sampling.cpp",
+    "src/llama-batch.cpp",
+    "src/llama-model.cpp",
+    "src/llama-adapter.cpp",
+    "src/llama-context.cpp",
+    "src/llama-arch.cpp",
+    "src/llama-chat.cpp",
+    "src/llama-cparams.cpp",
+    "src/llama-hparams.cpp",
+    "src/llama-impl.cpp",
+    "src/llama-kv-cache.cpp",
+    "src/llama-mmap.cpp",
+    "src/llama-model-loader.cpp",
+    "src/llama-quant.cpp",
     "src/unicode.cpp",
     "src/unicode-data.cpp",
     "ggml/src/ggml.c",
-    "ggml/src/ggml-aarch64.c",
     "ggml/src/ggml-alloc.c",
     "ggml/src/ggml-backend.cpp",
     "ggml/src/ggml-backend-reg.cpp",
     "ggml/src/ggml-cpu/ggml-cpu.c",
     "ggml/src/ggml-cpu/ggml-cpu.cpp",
     "ggml/src/ggml-cpu/ggml-cpu-traits.cpp",
-    "ggml/src/ggml-cpu/ggml-cpu-aarch64.c",
+    "ggml/src/ggml-cpu/ggml-cpu-aarch64.cpp",
     "ggml/src/ggml-cpu/ggml-cpu-quants.c",
     "ggml/src/ggml-threading.cpp",
     "ggml/src/ggml-quants.c",
@@ -35,6 +47,14 @@ var cSettings: [CSetting] =  [
     // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
     // .define("ACCELERATE_NEW_LAPACK"),
     // .define("ACCELERATE_LAPACK_ILP64")
+]
+
+var cxxSettings: [CXXSetting] = [
+    // Use at least C++14 (needed for std::make_unique, etc.)
+    .unsafeFlags(["-std=c++17", "-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
+    .unsafeFlags(["-fno-objc-arc"]),
+    .headerSearchPath("ggml/src"),
+    .headerSearchPath("ggml/src/ggml-cpu")
 ]
 
 #if canImport(Darwin)
@@ -78,16 +98,15 @@ let package = Package(
                 "models",
                 "tests",
                 "CMakeLists.txt",
-                "Makefile",
-                "ggml/src/ggml-metal/ggml-metal-embed.metal"
+                "Makefile"
             ],
             sources: sources,
             resources: resources,
             publicHeadersPath: "spm-headers",
             cSettings: cSettings,
+            cxxSettings: cxxSettings,
             linkerSettings: linkerSettings
         )
     ],
     cxxLanguageStandard: .cxx11
 )
-
